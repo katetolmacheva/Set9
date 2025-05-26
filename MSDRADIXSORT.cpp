4993b81c05921c3f9d43a7b1a40596bc0f80b71c
катя <156242171+katetolmacheva@users.noreply.github.com>
2025-05-26 05:18:04 +0300

    Create MSDRADIXSORT.cpp

---
+++ MSDRADIXSORT.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+ 
+const int asz = 257;
+ 
+int gch(const std::string& s, int d) {
+  if (d >= s.length()) {
+    return 0;
+  }
+  return static_cast<unsigned char>(s[d]) + 1;
+}
+ 
+void msdrs(std::vector<std::string>& a, int l, int r, int d) {
+  if (r <= l) return;
+ 
+  std::vector<std::string> aux(r - l + 1);
+  std::vector<int> cnt(asz, 0);
+ 
+  for (int i = l; i <= r; i++) {
+    cnt[gch(a[i], d)]++;
+  }
+ 
+  for (int i = 0; i < asz - 1; i++) {
+    cnt[i + 1] += cnt[i];
+  }
+ 
+  for (int i = r; i >= l; i--) {
+    aux[cnt[gch(a[i], d)] - 1] = a[i];
+    cnt[gch(a[i], d)]--;
+  }
+ 
+  for (int i = l; i <= r; i++) {
+    a[i] = aux[i - l];
+  }
+ 
+  int prev_cnt = 0;
+  for (int i = 0; i < asz; i++) {
+    if (cnt[i] > prev_cnt) {
+      msdrs(a, l + prev_cnt, l + cnt[i] - 1, d + 1);
+    }
+    prev_cnt = cnt[i];
+  }
+}
